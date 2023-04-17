aaa3a33f127615d2b9b829be5b291cb30797fbd8
phongvo27ams <phongvo27ams@gmail.com>
2023-04-17 22:06:07 +0700

    Create links with React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Outlet } from 'react-router-dom'
+
+import Header from './components/Header/Header'
+
 import './App.scss'
 
 const App = () => {
     return (
         <div className="app-container">
-            Hello, World!
+            <div className="header-container">
+                <Header />
+            </div>
+
+            <div className="main-container">
+                <div className="sidenav-container"></div>
+
+                <div className="app-content">
+                    <Outlet />
+                </div>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import App from './App'
+import Home from '../src/components/Home/Home'
+import User from '../src/components/User/User'
+import Admin from '../src/components/Admin/Admin'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />}>
+                <Route index element={<Home />} />
+                <Route path='/user' element={<User />} />
+                <Route path='/admin' element={<Admin />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
 )
